469adf18209bdac61b23707701527cd7d74d8f60
Lauri <laurianarodrigueslima@gmail.com>
2022-08-17 20:01:08 -0300

    refatoração

--- js/script.js
+++ js/script.js
@@ -12,19 +12,23 @@ form.onsubmit = (event) => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  const showAlertError = notANumber(weight) || notANumber(height)
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-  if (showAlertError) {
+  if (weightOrHeightIsNotANumber) {
     alertError.open()
     return;
   }
 
   alertError.close()
 
-  const result = IMC(weight, height)
+  const result = calculateIMC(weight, height)
+  
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
   const message = `Seu IMC é de ${result}`
 
   modal.message.innerText = message
   modal.open()
 }
-
