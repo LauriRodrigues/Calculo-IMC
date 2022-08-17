cc869324051a63a03b88e92e332f0b1ab0817aab
Lauri <laurianarodrigueslima@gmail.com>
2022-08-17 14:23:47 -0300

    object literals

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalCloseButton = document.querySelector('.close')
-const modalMessage = document.querySelector('.modal-message') 
+const modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal-message'),
+  closeButton: document.querySelector('.close'),
 
+  open() {
+    modal.wrapper.classList.add('open')
+  },
+  close() {
+    modal.wrapper.classList.remove('open')
+  },
+}
 
 form.onsubmit = (event) => {
   event.preventDefault()
@@ -16,12 +24,11 @@ form.onsubmit = (event) => {
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  modal.message.innerText = message
+  modal.open()
 }
 
-modalCloseButton.onclick = () => modalWrapper.classList.remove('open')
-
+modal.closeButton.onclick = () => modal.close()
 
 function IMC(weight, height) { 
   return (weight / ((height/100) ** 2)).toFixed(2)
